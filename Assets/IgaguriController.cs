9c01d42bcf09ec2b252ed60933823e8172a2827e
kouheigit <ookawakouhei777@gmail.com>
2023-06-13 00:35:15 +0900

    dev2

--- Assets/IgaguriController.cs
+++ Assets/IgaguriController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class IgaguriController : MonoBehaviour
 {
+    
     public void Shoot(Vector3 dir)
     {
         GetComponent<Rigidbody>().AddForce(dir);
@@ -12,6 +13,7 @@ public class IgaguriController : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
     }
     // Start is called before the first frame update
     void Start()
