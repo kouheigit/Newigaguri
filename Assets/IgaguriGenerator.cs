b2c8f7040f17100124bb57099b95845963911a3c
kouheigit <ookawakouhei777@gmail.com>
2023-06-14 21:42:10 +0900

    igaguri fix1

---
+++ Assets/IgaguriGenerator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriGenerator : MonoBehaviour
+{
+    public GameObject igaguriPrefab;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri = Instantiate(igaguriPrefab);
+
+            igaguri.GetComponent<IgaguriController>().Shoot(new Vector3(0, 200, 2000));
+        }
+    }
+}
